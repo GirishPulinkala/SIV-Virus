38c81bcf619dcb56a1b84e4f37b7d484031fdd53
GirishPulinkala <girish.pulinkala16@siesgst.ac.in>
2023-10-05 15:25:27 +0200

    'plots'

--- data_plots.R
+++ data_plots.R
@@ -39,3 +39,43 @@ text(b, tbl + 5, tbl, font=2)
 
 
 #########################################
+#read count matrix
+count_data<-fread('C://Users//giris//Downloads//count_matrix_renamed.csv')
+View(count_data)
+
+#Subsetting data based on timepoints for normalization and further steps
+D0<-subset(count_data,select=c(V1,grep("W0_D0|_[0-9]+_D0",colnames(count_data))))
+D3<-subset(count_data,select=c(V1,grep("W0_D3|_[0-9]+_D3",colnames(count_data))))
+D7<-subset(count_data,select=c(V1,grep("W0_D7|_[0-9]+_D7",colnames(count_data))))
+D126<-subset(count_data,select=c(V1,grep("W18_D0|_[0-9]+_D98|_[0-9]+_D126",colnames(count_data))))
+D129<-subset(count_data,select=c(V1,grep("W18_D3|_[0-9]+_D98|_[0-9]+_D129",colnames(count_data))))
+D133<-subset(count_data,select=c(V1,grep("W18_D7|_[0-9]+_D98|_[0-9]+_D133",colnames(count_data))))
+prechal<-subset(count_data,select=c(V1,grep("W88_D0|_[0-9]+_D462|_[0-9]+_D455|_[0-9]+_D538|_[0-9]+_D448|_[0-9]+_D540",colnames(count_data))))
+
+
+
+#STANDARD DEVIATION
+
+animal<-subset(count_data,select=(grep('29523',colnames(count_data))))
+animal<-subset(count_data,select=-c(v1))
+animal
+
+x<-as.numeric(animal[36,])
+sd(x)
+mean(x)
+y<-dnorm(x, mean(x), sd(x))
+
+D0<<-D0[,1] 
+df<-t(D0)
+View(df)
+
+plot(x, y, type = "l", lwd = 2, col = "blue", ylab = "probability", xlab = "x")
+
+t(sapply(split.default(df, names(df)), function(x)  {
+  x1 <- unlist(x)
+  data.frame(mean = mean(x1, na.rm = TRUE), sd = sd(x1, na.rm = TRUE))}))
+
+
+
+
+
